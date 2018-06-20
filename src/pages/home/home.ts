187e5be38ba50c764a589179fce711fa41efa03e
Trevor Bedsaul <tdb7mw@virginia.edu>
2018-06-20 10:49:43 +0200

    Started transferring information to profile page

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,13 +2,15 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { LoginPage } from '../login/login';
 import { RegisterPage } from '../register/register';
+import { Storage } from '@ionic/storage';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public storage: Storage) {
 
   }
 
--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -4,6 +4,9 @@ import { NavParams } from 'ionic-angular';
 import { App } from 'ionic-angular';
 import { ToastController } from 'ionic-angular';
 import { HomePage } from '../home/home';
+import { Storage } from '@ionic/storage';
+import { Http } from '@angular/http';
+
 @Component({
     selector: 'page-profile',
     templateUrl: 'profile.html'
@@ -11,25 +14,30 @@ import { HomePage } from '../home/home';
 export class ProfilePage {
 
     name: string;
+    first: string;
+    last: string;
+    token: string;
 
-    constructor(public navCtrl: NavController, public navParams: NavParams, private app: App, public toastCtrl: ToastController) {
-        // storage.get('token').then((val) => {
-        //     this.token = val;
-        //     this.http
-        //         .get("http://localhost:3000/users", {
-        //             params: {
-        //                 token: this.token
-        //             }
-        //         })
-        //         .subscribe(
-        //             result => {
-        //                 this.user_id = result.json().user_id;
-        //             },
-        //             error => {
-        //                 console.log(error);
-        //             }
-        //         );
-        // });
+    constructor(public http: Http, public navCtrl: NavController, public navParams: NavParams, private app: App, public toastCtrl: ToastController, private storage: Storage) {
+        storage.get('token').then((val) => {
+            this.token = val;
+            this.http
+                .get("http://localhost:3000/users", {
+                    params: {
+                        token: this.token
+                    }
+                })
+                .subscribe(
+                    result => {
+                        this.first = result.json().firstname;
+                        this.last = result.json().lastname;
+                        this.name = this.first + " " + this.last;
+                    },
+                    error => {
+                        console.log(error);
+                    }
+                );
+        });
     }
 
     navigateToHome() {
